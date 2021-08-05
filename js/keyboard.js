fc2541c0aaa67b01154e0f8c05f135fc038c3c94
arth-b10 <88114647+arth-b10@users.noreply.github.com>
2021-08-05 23:56:36 +0200

    Update keyboard.js

--- js/keyboard.js
+++ js/keyboard.js
@@ -4,12 +4,12 @@ $(function(){
 		onDocumentTouchMove.x = e.changedTouches[e.changedTouches.length - 1].clientX;
     		onDocumentTouchMove.y = e.changedTouches[e.changedTouches.length - 1].clientY;
 	}
-
+/*
 	function onDocumentTouchEnd(e){
 		e.preventDefault();
     		var elem = document.elementFromPoint(onDocumentTouchMove.x, onDocumentTouchMove.y);
    		$write.html($write.html() + elem.id);
-	}
+	}*/
 	
 	document.ontouchmove = function(e){
     		e.preventDefault();
@@ -34,7 +34,108 @@ $(function(){
 	});*/
 	$(document).on("touchmove", onDocumentTouchMove);
 	
-	$(document).on("touchend", onDocumentTouchEnd);
+	$(document).on("touchend", function(e){
+		e.preventDefault();
+		
+    		var elem = document.elementFromPoint(onDocumentTouchMove.x, onDocumentTouchMove.y);
+		var character;
+		var clickedItem = elem.id
+		
+		switch (clickedItem) {
+      case "Enter":
+          $('#Keyboard').hide();
+          character="";
+        break;
+      case "Return":
+	  var html = $write.html();
+	  $write.html(html.substr(0, html.length - 1));
+	  character="";
+        break;
+      case "A":
+          character = "A";
+        break;  
+      case "B":
+          character = "B";
+        break;
+      case "C":
+          character = "C";
+        break;
+      case "D":
+          character = "D";
+        break;  
+      case "E":
+          character = "E";
+        break;
+      case "F":
+          character = "F";
+        break;
+      case "G":
+          character = "G";
+        break;  
+      case "H":
+          character = "H";
+        break;
+      case "I":
+          character = "I";
+        break;
+      case "J":
+          character = "J";
+        break;  
+      case "K":
+          character = "K";
+        break;
+      case "L":
+          character = "L";
+        break;
+      case "M":
+          character = "M";
+        break;  
+      case "N":
+          character = "N";
+        break;
+      case "O":
+          character = "O";
+        break;
+      case "P":
+          character = "P";
+        break;  
+      case "Q":
+          character = "Q";
+        break;
+      case "R":
+          character = "R";
+        break;
+      case "S":
+          character = "S";
+        break;  
+      case "T":
+          character = "T";
+        break;
+      case "U":
+          character = "U";
+        break;
+      case "V":
+          character = "V";
+        break;  
+      case "W":
+          character = "W";
+        break;
+      case "X":
+          character = "X";
+        break;
+      case "Y":
+          character = "Y";
+        break;  
+      case "Z":
+          character = "Z";
+        break;
+      default:
+       	  character = "";
+       }
+		
+		
+   		$write.html($write.html() + character);
+	});
 /*
 	document
 	.getElementById("Enter")
