2ea8d5cea9c75a5916b66d5acd9123629849ac51
arth-b10 <88114647+arth-b10@users.noreply.github.com>
2021-08-06 11:38:55 +0200

    Update keyboard.js

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,10 +1,10 @@
 $(function(){
-	/*
+	
 	function onDocumentTouchMove(e){
 		e.preventDefault();
 		onDocumentTouchMove.x = e.touches[e.touches.length - 1].clientX;
     		onDocumentTouchMove.y = e.touches[e.touches.length - 1].clientY;
-	}*/
+	};
 	/*
 	function onDocumentTouchEnd(e){
 		e.preventDefault();
@@ -38,8 +38,8 @@ $(function(){
 	$('.key').on("touchend", function(e){
 		e.preventDefault();
 		
-		onDocumentTouchMove.x = e.changedTouches[e.changedTouches.length].clientX;
-    		onDocumentTouchMove.y = e.changedTouches[e.changedTouches.length].clientY;
+		onDocumentTouchMove.x = e.changedTouches[e.changedTouches.length - 1].clientX;
+    		onDocumentTouchMove.y = e.changedTouches[e.changedTouches.length - 1].clientY;
 		
     		var elem = document.elementFromPoint(onDocumentTouchMove.x, onDocumentTouchMove.y);
 		var character;
